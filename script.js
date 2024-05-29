bd331e1f4efd40cadd8d3d109e4183e2dfe0ce40
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-05-29 19:41:15 +0200

    Advance

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello, World!");
+document.addEventListener("DOMContentLoaded", () => {
+  const answer = document.querySelectorAll("p");
+  const question = document.querySelectorAll("h1");
+
+  const generateBinary = () => {
+    const binary = Math.floor(Math.random() * 2);
+    const binary1 = Math.floor(Math.random() * 2);
+    const binary2 = Math.floor(Math.random() * 2);
+    const binary3 = Math.floor(Math.random() * 2);
+    return [binary, binary1, binary2, binary3];
+  };
+
+  console.log(generateBinary().join(",").toString());
+  question[0].innerHTML = generateBinary()
+    .join(",")
+    .toString.replaceAll(",", "");
+});
