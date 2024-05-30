00d6dc82c8ad17ca40138a1cf3a9503f7ee6e1c2
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-05-30 15:30:36 +0200

    chore: Update HTML and JavaScript for hexadecimal conversion form

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
   const answer = document.querySelectorAll("p");
   const question = document.querySelectorAll("h1");
+  const form = document.querySelector("form");
+  const input = document.querySelector("input");
+
+  const checkAnswer = {
+    0: "0000",
+    1: "0001",
+    2: "0010",
+    3: "0011",
+    4: "0100",
+    5: "0101",
+    6: "0110",
+    7: "0111",
+    8: "1000",
+    9: "1001",
+    A: "1010",
+    B: "1011",
+    C: "1100",
+    D: "1101",
+    E: "1110",
+    F: "1111",
+  };
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const inputUser = input.value;
+
+    input.value = "";
+
+    console.log(inputUser);
+  });
 
   const generateBinary = () => {
     const binary = Math.floor(Math.random() * 2);
@@ -10,8 +41,10 @@ document.addEventListener("DOMContentLoaded", () => {
     return [binary, binary1, binary2, binary3];
   };
 
-  console.log(generateBinary().join(",").toString());
-  question[0].innerHTML = generateBinary()
+  const binaryQuestion = generateBinary()
     .join(",")
-    .toString.replaceAll(",", "");
+    .toString()
+    .replaceAll(",", "");
+
+  question[0].innerHTML = binaryQuestion;
 });
