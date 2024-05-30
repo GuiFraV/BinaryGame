c2230873add847002d7d914ca34afcb5d7b274b3
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-05-30 16:19:10 +0200

    refactor: Simplify hexadecimal conversion logic and improve form submission handling

--- script.js
+++ script.js
@@ -3,22 +3,25 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form");
   const input = document.querySelector("input");
   const answer = document.querySelector("p");
+  const counter = document.querySelector("span");
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  let time = 3;
+  let interval;
 
-    const inputUser = input.value.toUpperCase();
-    input.value = "";
-
-    const binaryQuestion = question[0].innerHTML;
-    const hexQuestion = parseInt(binaryQuestion, 2).toString(16).toUpperCase();
-
-    if (inputUser === hexQuestion) {
-      answer.innerHTML = "Correct!";
-    } else {
-      answer.innerHTML = "Incorrect. The correct answer is " + hexQuestion;
-    }
-  });
+  const startCounter = () => {
+    counter.style.display = "inline";
+    counter.innerHTML = time;
+    interval = setInterval(() => {
+      time--;
+      counter.innerHTML = time;
+      if (time === 0) {
+        clearInterval(interval);
+        counter.style.display = "none";
+        time = 3;
+        setNewQuestion();
+      }
+    }, 1000);
+  };
 
   const generateBinary = () => {
     const binary = Math.floor(Math.random() * 2);
@@ -28,10 +31,31 @@ document.addEventListener("DOMContentLoaded", () => {
     return [binary, binary1, binary2, binary3];
   };
 
-  const binaryQuestion = generateBinary()
-    .join(",")
-    .toString()
-    .replaceAll(",", "");
+  const setNewQuestion = () => {
+    const binaryQuestion = generateBinary()
+      .join(",")
+      .toString()
+      .replaceAll(",", "");
+    question[0].innerHTML = binaryQuestion;
+  };
 
-  question[0].innerHTML = binaryQuestion;
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const inputUser = input.value.toUpperCase();
+    input.value = "";
+
+    const binaryQuestion = question[0].innerHTML;
+    const hexQuestion = parseInt(binaryQuestion, 2).toString(16).toUpperCase(); // Convertir la question binaire en hexadécimal
+
+    if (inputUser === hexQuestion) {
+      answer.innerHTML = "Correct!";
+      startCounter();
+    } else {
+      answer.innerHTML = "Incorrect. The correct answer is " + hexQuestion;
+    }
+  });
+
+  setNewQuestion();
+  counter.style.display = "none";
 });
